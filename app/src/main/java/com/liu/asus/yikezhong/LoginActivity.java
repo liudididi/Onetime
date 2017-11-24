2618f64d89549c9f1404f8a92fb79583265d6cce
liudididi <31385116+liudididi@users.noreply.github.com>
2017-11-24 10:22:46 +0800

    QQ完善

--- app/src/main/java/com/liu/asus/yikezhong/LoginActivity.java
+++ app/src/main/java/com/liu/asus/yikezhong/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.liu.asus.yikezhong;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+
+import com.tencent.connect.UserInfo;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 import java.util.List;
 
@@ -26,7 +33,9 @@ public class LoginActivity extends BaseActivity {
     RelativeLayout relaQq;
     @BindView(R.id.login_tv)
     TextView loginTv;
-
+    private Tencent mTencent;
+    private UserInfo mInfo;
+    private QQLoginListener mListener;
     @Override
     public List<Basepresent> initp() {
         return null;
@@ -41,9 +50,16 @@ public class LoginActivity extends BaseActivity {
     public void init() {
         ButterKnife.bind(this);
 
+        mListener = new QQLoginListener();
+        if (mTencent == null) {
+            mTencent = Tencent.createInstance("1104796216", this);
+        }
 
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mTencent.onActivityResultData(requestCode, resultCode, data, mListener);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +69,37 @@ public class LoginActivity extends BaseActivity {
 
     @OnClick(R.id.rela_qq)
     public void onViewClicked() {
-       Toast("登录QQ");
+     /*  Toast("登录QQ");
+       intent(LoginActivity.this,MainActivity.class);*/
+
+        if (!mTencent.isSessionValid()) {
+            mTencent.login(this, "all", mListener);
+        }
+    }
+
+    private class QQLoginListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object object) { //登录成功
 
 
+            Toast("登录成功");
+                 /*   //获取openid和token
+            initOpenIdAndToken(object);
+            //获取用户信息
+            getUserInfo();*/
+        }
+
+        @Override
+        public void onError(UiError uiError) {  //登录失败
+            Toast(""+uiError.toString());
+        }
+
+        @Override
+        public void onCancel() {
+
+            //取消登录
+        }
     }
 }
+
